869ae777f6f8bc4daf9075df6d78027dfe729b73
Alexandre Bléron <alex.bleron@gmail.com>
2018-03-22 17:57:35 +0100

    Removed unused copy of assimp-rs in the repo

--- ext/assimp-sys/src/types/vector2.rs
+++
@@ -1,8 +0,0 @@
-use std::os::raw::c_float;
-
-#[repr(C, packed)]
-#[derive(Clone, Copy, Debug, PartialEq)]
-pub struct AiVector2D {
-    pub x: c_float,
-    pub y: c_float,
-}
--- ext/assimp-sys/src/types/vector3.rs
+++
@@ -1,9 +0,0 @@
-use std::os::raw::c_float;
-
-#[repr(C, packed)]
-#[derive(Clone, Copy, Debug, PartialEq)]
-pub struct AiVector3D {
-    pub x: c_float,
-    pub y: c_float,
-    pub z: c_float,
-}
